2883771856b35e2fdef286b07d409bae15d37734
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-02-05 23:33:56 -0800

    feet: Add project structure
    Create the project on Xcode

---
+++ VirtualTourist/VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Tabassum Tamanna on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
